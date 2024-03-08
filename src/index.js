e7a4298af05ff6ccdf9fdff53514b028050c2cfa
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-03-09 01:46:36 +0400

    add task class and export a function to create tasks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
+import {createTask} from './tasksLogic.js'
 
 console.log('Some update');
 console.log('hi');
+
+createTask('hi', 'hi', 'hi', 'hi', 'hi');
+createTask('rf', 'fh', 'fds', 'fds', 'fd')
