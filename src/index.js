dfa821297d352df7ca1a751fac4bb930967d4be4
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-04-24 01:12:02 +0400

    decided to start with scratch

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './style.css';
-import {createTask} from './tasksLogic.js'
-
-console.log('Some update');
-console.log('hi');
-
-createTask('hi', 'hi', 'hi', 'hi', 'hi');
-createTask('rf', 'fh', 'fds', 'fds', 'fd')
