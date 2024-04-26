8761992a86b487b344ead3b80aa68502d1844350
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-04-26 16:20:34 +0400

    create an initial setup to work with

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo",
+      template: "src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+    },
+    {
+      test: /\.html$/i,
+      loader: "html-loader",
+    },
+    {
+      test: /\.(png||jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+    ],
+  },
+};
