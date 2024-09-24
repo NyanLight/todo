3ec4b8a7fe7ee25fa75e01dd3f45fb6b04402f29
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-09-24 14:13:08 +0400

    add bin functionality to projects list

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,5 +1,6 @@
-import { createProject } from "./controller";
+import { createProject, deleteProject } from "./controller";
 import { projects } from "./projects";
+import binIcon from '../assets/rubbish-bin-svgrepo-com.svg';
 
 const addProjectBtn = document.querySelector('#addProjectBtn');
 addProjectBtn.addEventListener('click', () => {
@@ -9,18 +10,34 @@ addProjectBtn.addEventListener('click', () => {
     displayProjects();
 }) 
 
+
 export function displayProjects() {
     const list = document.querySelector('#projectsList')
     list.innerHTML = '';
     for (const project of projects) {
         const li = document.createElement('li');
+        li.classList.toggle('projectLi');
         const div = document.createElement('div');
         const span = document.createElement('span');
+        const bin = document.createElement('img');
         div.classList.toggle('projectDiv');
-        span.classList.toString('projectSpan');
+        span.classList.toggle('projectSpan');
         span.textContent = project.name;
+        bin.src = binIcon;
+        bin.classList.toggle('noneBin');
+        bin.addEventListener('click', () => {
+            deleteProject(span.textContent);
+            displayProjects();
+        })
         li.appendChild(div);
         div.appendChild(span);
+        div.appendChild(bin);
         list.appendChild(li);
-    }
+        li.addEventListener('mouseover', () => {
+            bin.classList.toggle('Bin');
+        })
+        li.addEventListener('mouseout', () => {
+            bin.classList.toggle('Bin');
+        })
+    }   
 }
