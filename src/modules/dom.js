d854b3e26752b31fed8efc19ce10ba7d3c7bd3c8
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-10-05 17:00:14 +0400

    add modal dialog to add create new task

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,6 +1,12 @@
-import { createProject, deleteProject, getProjectIndex } from "./controller";
+import {
+  createProject,
+  createTask,
+  deleteProject,
+  getProjectIndex,
+} from "./controller";
 import { projects } from "./projects";
 import binIcon from "../assets/rubbish-bin-svgrepo-com.svg";
+import { add } from "date-fns";
 
 const addProjectBtn = document.querySelector("#addProjectBtn");
 addProjectBtn.addEventListener("click", () => {
@@ -10,25 +16,44 @@ addProjectBtn.addEventListener("click", () => {
   displayProjects();
 });
 
+const openDialogBtn = document.getElementById("openDialogBtn");
+const dialog = document.getElementById("dialog");
+openDialogBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+addTaskBtn.addEventListener("click", () => {
+  const project = document.getElementById("projectInput").value;
+  const title = document.getElementById("titleInput").value;
+  const description = document.getElementById("descriptionInput").value;
+  const dueDate = document.getElementById("dateInput").value;
+  const priority = document.getElementById("priorityInput").value;
+  createTask(project, title, description, dueDate, priority);
+  dialog.close();
+  displayProject(project);
+});
+
 export function displayProject(projectName) {
   const tasksList = document.getElementById("tasksList");
+  tasksList.innerHTML = "";
   const index = getProjectIndex(projectName);
   const currentProject = document.getElementById("currentProject");
   currentProject.textContent = projects[index].name;
   for (const task of projects[index].tasks) {
-    const li = document.createElement('li');
-    const title = document.createElement('div');
-    const description = document.createElement('div');
-    const dueDate = document.createElement('div');
-    const priority = document.createElement('div');
-    li.classList.toggle('taskLi');
-    title.classList.toggle('titleDiv');
-    description.classList.toggle('descriptionDiv');
-    dueDate.classList.toggle('dueDateDiv');
-    priority.classList.toggle('priorityDiv');
+    const li = document.createElement("li");
+    const title = document.createElement("div");
+    const description = document.createElement("div");
+    const dueDate = document.createElement("div");
+    const priority = document.createElement("div");
+    li.classList.toggle("taskLi");
+    title.classList.toggle("titleDiv");
+    description.classList.toggle("descriptionDiv");
+    dueDate.classList.toggle("dueDateDiv");
+    priority.classList.toggle("priorityDiv");
     title.textContent = task.title;
     description.textContent = task.description;
-    dueDate.textContent = task.dueDate; 
+    dueDate.textContent = task.dueDate;
     priority.textContent = task.priority;
     li.appendChild(title);
     li.appendChild(description);
@@ -48,9 +73,9 @@ export function displayProjects() {
     div.classList.toggle("projectDiv");
     span.classList.toggle("projectSpan");
     span.textContent = project.name;
-    span.addEventListener('click',() => {
+    span.addEventListener("click", () => {
       displayProject(span.textContent);
-    })
+    });
     li.appendChild(div);
     div.appendChild(span);
     list.appendChild(li);
