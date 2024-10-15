4380b4e02fb728ddafa87c809d60b3145ca974e4
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-10-16 00:44:50 +0400

    build

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './styles.css';
-import { completeTask, createProject, createTask, deleteProject, deleteTask, initialization } from './modules/controller';
-import { projects } from './modules/projects';
-
-initialization();
